57418d36523f4c78aa50bb42d8468816ed3f6148
Dylan Simpson <65699922+DylanJosephSimpson@users.noreply.github.com>
2020-08-13 18:05:40 +1200

    Create Main.java

---
+++ CluedoGame/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create the Frame by making a new CluedoGui
+        JFrame frame = new CluedoGUI("Cluedo Game");
+
+        frame.setSize(720, 480);
+        frame.setVisible(true);
+    }
+
+}
