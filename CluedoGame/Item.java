054f5102b92e6c634b45c23a1bca56e8c8b32521
Iqbal Wan <54200374+iqbalw@users.noreply.github.com>
2020-08-19 05:36:13 +1200

    Added Item interface class

---
+++ CluedoGame/Item.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+/**
+ * Interface class for the items in the game
+ *
+ * @author Iqbal
+ */
+public interface Item {
+
+    /**
+     * Draws the item on the board
+     * @param g Graphics object
+     * @param x x-coordinate of which tile it will be located on
+     * @param y y-coordinate of which tile it will be located on
+     */
+    public void draw(Graphics g, int x, int y);
+
+    /**
+     * Erases the item from the board.
+     * This method uses the x and y arguments to locate where to erase the item.
+     * @param g Graphics object
+     */
+    public void erase(Graphics g);
+
+}
