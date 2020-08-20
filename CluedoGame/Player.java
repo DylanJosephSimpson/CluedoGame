243feb0aeaad7bd3b7a57ef05b6f9ab0328d832d
Iqbal Wan <54200374+iqbalw@users.noreply.github.com>
2020-08-21 11:42:58 +1200

    Constructor and fields added

--- CluedoGame/Player.java
+++ CluedoGame/Player.java
@@ -4,4 +4,19 @@
  *
  */
 public class Player {
+
+    /**
+     * Corresponds to one of the 6 playable characters in the game
+     */
+    private Character assignedCharacter;
+
+    /**
+     * Name of the player
+     */
+    private String name;
+
+    public Player(Character assignedCharacter, String name) {
+        this.assignedCharacter = assignedCharacter;
+        this.name = name;
+    }
 }
