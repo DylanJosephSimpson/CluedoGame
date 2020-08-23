31bfc6f03a240caa5150621ff9f1266205ae462c
Iqbal Wan <54200374+iqbalw@users.noreply.github.com>
2020-08-23 15:32:24 +1200

    Added basic components for the class
    May need modifications later to fit the game's needs

--- CluedoGame/Room.java
+++ CluedoGame/Room.java
@@ -3,4 +3,22 @@
  * what is stored inside of rooms.
  */
 public class Room {
+
+    /**
+     * Name of the room
+     */
+    private String roomName;
+
+    /**
+     * Constructor for creating a room
+     * @param roomName Name of the room
+     */
+    public Room(String roomName){
+        this.roomName = roomName;
+    }
+
+    @Override
+    public String toString() {
+        return roomName;
+    }
 }
