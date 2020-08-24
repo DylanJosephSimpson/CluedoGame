855b091d1f1c0ad4341d3e8917a967baeef8a189
Dylan Simpson <65699922+DylanJosephSimpson@users.noreply.github.com>
2020-08-24 16:01:39 +1200

    Update Character.java

--- CluedoGame/Character.java
+++ CluedoGame/Character.java
@@ -1,153 +1,150 @@
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Arc2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 /**
- * The character class is responsible for keeping track
- * of the player's character as it traverses the board.
- */
-public class Character implements Item {
-
-    /**
-     * Name of the character
+     * The character class is responsible for keeping track
+     * of the player's character as it traverses the board.
      */
-    private String characterName;
+    public class Character implements Item {
 
+        /**
+         * Name of the character
+         */
+        private String characterName;
 
-    /**
-     * x-coordinate of where to draw on the board
-     */
-    private int x;
+        /**
+         * x-coordinate of where to draw on the board
+         */
+        private int x;
 
-    /**
-     * y-coordinate of where to draw on the board
-     */
-    private int y;
+        /**
+         * y-coordinate of where to draw on the board
+         */
+        private int y;
 
-    /**
-     * Room that the player is currently in. By default it is null as it does not spawn in one.
-     */
-    private Room currentRoom = null;
+        /**
+         * Room that the player is currently in. By default it is null as it does not spawn in one.
+         */
+        private Room currentRoom = null;
 
-    /**
-     * Constructor for character
-     *
-     * @param characterName Name of the weapon
-     */
-    public Character(String characterName , int x, int y) {
-        this.characterName = characterName;
-        this.x = x ;
-        this.y = y;
-    } //todo add prams for starting pos
+        /**
+         * Constructor for character
+         *
+         * @param characterName Name of the weapon
+         */
+        public Character(String characterName , int x, int y) {
+            this.characterName = characterName;
+            this.x = x ;
+            this.y = y;
+        } //todo add prams for starting pos
 
-    /**
-     * When a player moves in a certain direction the x or y position changes for their character and then board is redrawn
-     *
-     * @param dir
-     */
-    public void move(String dir){
-        if(dir.equals("NORTH")){
-            y-=30;
-        }else if(dir.equals("EAST")){
-            x+=30;
-        } else if(dir.equals("SOUTH")){
-            y+=30;
-        } else if(dir.equals("WEST")){
-            x-=30;
+        /**
+         * When a player moves in a certain direction the x or y position changes for their character and then board is redrawn
+         *
+         * @param dir
+         */
+        public void move(String dir){
+            if(dir.equals("NORTH")){
+                y-=30;
+            }else if(dir.equals("EAST")){
+                x+=30;
+            } else if(dir.equals("SOUTH")){
+                y+=30;
+            } else if(dir.equals("WEST")){
+                x-=30;
+            }
         }
-    }
 
-    @Override
-    public void draw(Graphics g, int x, int y) {
+        @Override
+        public void draw(Graphics g, int x, int y) {
 
-        Graphics2D g2d = (Graphics2D) g;
-        this.x = x;
-        this.y = y;
+            Graphics2D g2d = (Graphics2D) g;
+            this.x = x;
+            this.y = y;
 
-        String path = "CharacterPieces/" + characterName + ".png";
-        try {
-            BufferedImage image = ImageIO.read(new File(path));
-            Image scaledImage = image.getScaledInstance(28, 28, Image.SCALE_SMOOTH);
-            g2d.drawImage(scaledImage,x+1,y+1,null);
-        } catch (IOException e) {
-            System.out.println("Error: Image drawing for " + characterName + " failed.");
+            String path = "CharacterPieces/" + characterName + ".png";
+            try {
+                BufferedImage image = ImageIO.read(new File(path));
+                Image scaledImage = image.getScaledInstance(28, 28, Image.SCALE_SMOOTH);
+                g2d.drawImage(scaledImage,x+1,y+1,null);
+            } catch (IOException e) {
+                System.out.println("Error: Image drawing for " + characterName + " failed.");
+            }
         }
-    }
 
-    @Override
-    public void erase(Graphics g) {
-        g.setColor(Color.WHITE);
-        g.fillRect(x+1,y+1,28,28);
-        //todo - Caleb - erase this properly
-    }
+        @Override
+        public void erase(Graphics g) {
+            g.setColor(Color.WHITE);
+            g.fillRect(x+1,y+1,28,28);
+            //todo - Caleb - erase this properly
+        }
 
-    /**
-     * When a suggestion is made this character moves to the designated room
-     * @param room Room the character is moving to
-     */
-    public void moveToRoom(Room room){
-        //TODO: Write this method once the Room class is implemented (Iqbal)
-    }
+        /**
+         * When a suggestion is made this character moves to the designated room
+         * @param room Room the character is moving to
+         */
+        public void moveToRoom(Room room){
+            //TODO: Write this method once the Room class is implemented (Iqbal)
+        }
 
-    /**
-     * Getter for currentRoom
-     * @return
-     */
-    public Room getCurrentRoom() {
-        return currentRoom;
-    }
+        /**
+         * Getter for currentRoom
+         * @return
+         */
+        public Room getCurrentRoom() {
+            return currentRoom;
+        }
 
-    /**
-     * Setter for currentRoom
-     * @param currentRoom
-     */
-    public void setCurrentRoom(Room currentRoom) {
-        this.currentRoom = currentRoom;
-    }
+        /**
+         * Setter for currentRoom
+         * @param currentRoom
+         */
+        public void setCurrentRoom(Room currentRoom) {
+            this.currentRoom = currentRoom;
+        }
 
-    @Override
-    public String toString() {
-        return characterName;
-    }
+        @Override
+        public String toString() {
+            return characterName;
+        }
 
-    /**
-     * Getter for x position
-     *
-     * @return
-     */
-    public int getX() {
-        return x;
-    }
+        /**
+         * Getter for x position
+         *
+         * @return
+         */
+        public int getX() {
+            return x;
+        }
 
-    /**
-     * Getter for y position
-     *
-     * @return
-     */
-    public int getY() {
-        return y;
-    }
+        /**
+         * Getter for y position
+         *
+         * @return
+         */
+        public int getY() {
+            return y;
+        }
 
-    /**
-     * Setter for x position
-     *
-     * @param x
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
+        /**
+         * Setter for x position
+         *
+         * @param x
+         */
+        public void setX(int x) {
+            this.x = x;
+        }
 
-    /**
-     * Setter for y position
-     *
-     * @param y
-     */
-    public void setY(int y) {
-        this.y = y;
-    }
-}
+        /**
+         * Setter for y position
+         *
+         * @param y
+         */
+        public void setY(int y) {
+            this.y = y;
+        }
 
+    }
