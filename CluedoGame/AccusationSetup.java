5ecefb15a7f2f46a39f4437e6ea3c807a3e84f50
Dylan Simpson <65699922+DylanJosephSimpson@users.noreply.github.com>
2020-08-24 23:16:33 +1200

    Create AccusationSetup.java

---
+++ CluedoGame/AccusationSetup.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class AccusationSetup {
+
+    private JPanel MakeAccusationWindow;
+
+    String[] Weapon = new String[]{"Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Spanner"};
+    private JLabel WeaponBoxDesc = new JLabel("Weapon Choice");
+    private JComboBox<String> WeaponSelection = new JComboBox<>(Weapon);
+
+    String[] Character =  new String[]{"Miss Scarlett", "Colonel Mustard", "Mr. Green", "Ms. White", "Mrs. Peacock", "Professor Plum"};
+    private JLabel CharacterBoxDesc = new JLabel("Character Choice");
+    private JComboBox<String> CharacterSelection  = new JComboBox<>(Character);
+
+    String[] Room = new String[]{"Kitchen", "Ball Room", "Conservatory", "Billiard Room", "Library", "Study", "Hall", "Lounge", "Dining Room"};
+    private JLabel RoomBoxDesc = new JLabel("Weapon Choice");
+    private JComboBox<String> RoomSelection  = new JComboBox<>(Room);
+
+    public AccusationSetup(){
+
+        MakeAccusationWindow = new JPanel();
+
+        GridLayout layout = new GridLayout(3, 2);
+
+        MakeAccusationWindow.setLayout(layout);
+
+        MakeAccusationWindow.add(WeaponBoxDesc);
+        MakeAccusationWindow.add(WeaponSelection);
+        MakeAccusationWindow.add(CharacterBoxDesc);
+        MakeAccusationWindow.add(CharacterSelection);
+        MakeAccusationWindow.add(RoomBoxDesc);
+        MakeAccusationWindow.add(RoomSelection);
+
+        int result = JOptionPane.showConfirmDialog(null, MakeAccusationWindow,
+                "Make an Accusation", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION){
+
+            // TODO ACCUSATION LOGIC GO HERE BRRR NO TOUCH!
+
+            
+            System.out.println(WeaponSelection.getSelectedItem() + "\n" + CharacterSelection.getSelectedItem() + "\n"
+            + RoomSelection.getSelectedItem() + "\n");
+        }
+    }
+
+}
