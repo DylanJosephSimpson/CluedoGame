8e94837ac97ea1ef616338916652ebb9bdd892c4
Dylan Simpson <65699922+DylanJosephSimpson@users.noreply.github.com>
2020-08-24 16:02:43 +1200

    Update Main.java

--- CluedoGame/Main.java
+++ CluedoGame/Main.java
@@ -1,31 +1,11 @@
-import javax.swing.*;
+
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import javax.swing.*;
 
-public class Main {
+public class CluedoMain {
 
-
-    public static void main(String[] args) {
-        int GAME_WIDTH = 720;
-        int GAME_HEIGHT = 885;
-
-        boolean setupIsDone = true; //FIXME: set this false once player creation is done
-
-        JFrame setupFrame = new PlayerCreationMenu("Cluedo: How many players?");
-        setupFrame.setSize(GAME_WIDTH, GAME_HEIGHT/2);
-        //Actual game starts here
-
-        if (setupIsDone) {
-            // Create the Frame by making a new CluedoGui
-            Board b = new Board();
-            JFrame frame = new CluedoGUI("Cluedo Game",b);
-            frame.setSize(GAME_WIDTH, GAME_HEIGHT);
-            frame.setVisible(true);
-        }
-
+    public static void main(String[] args){
+        new MenuSetup("Cluedo");
     }
-
 }
+
