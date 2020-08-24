162ac4596b1c3e306cb6e810c1084aa87eecc632
Praveen Bandarage <51874907+PraveentheMachine@users.noreply.github.com>
2020-08-24 12:43:01 +1200

    Adding basis for accusatioin
    I will need to implement the GUI element, first and foremost we must ensure that the turn-based system is ready to handle accusations

--- CluedoGame/Player.java
+++ CluedoGame/Player.java
@@ -1,7 +1,6 @@
 /**
  * The player class is responsible for allowing a user to interact with
  * the board and game.
- *
  */
 public class Player {
 
@@ -15,9 +14,51 @@ public class Player {
      */
     private String assignedCharacter;
 
+    /**
+     * Boolean which represents if the player is in game
+     */
+    private boolean activePlayer;
+
+    private Tile currentTile;
+
     public Player(String name, String assignedCharacter) {
         this.name = name;
         this.assignedCharacter = assignedCharacter;
+    }
+
+    /**
+     *Method for making an accusation
+     **/
+
+    public void makeAccusation(CharacterCard characterCard, WeaponCard weaponCard, RoomCard roomCard) {
+        if (!activePlayer || !isInARoom()) {
+            return; //If the player is not active due to making a false accusation or not in a room there is no
+            // point in making an accusation
+        }
+        System.out.println("Accusation is being made");
+
+
+        //False accusation as the envelope does not contain all three of the cards within the envelope
+        if (!Board.envelope.contains(characterCard) || !Board.envelope.contains(weaponCard) || !Board.envelope.contains(roomCard)) {
+            System.out.println("This is a false accusation ");
+            activePlayer = false;
+
+        }
+        //A successful accusation has been made and the game has been completed
+        else {
+            System.out.println("Game is completed, a successful accusation has card ");
+        }
+
+    }
+
+
+    /**
+     * Method that checks if the player is in a room
+     *
+     * @return
+     */
+    private boolean isInARoom() {
+        return true;
     }
 
     @Override
