a40968d56177b22db43692f9357acd46d8b594a6
Praveen Bandarage <51874907+PraveentheMachine@users.noreply.github.com>
2020-08-27 00:16:20 +1200

    Update Weapon.java

--- CluedoGame/Weapon.java
+++ CluedoGame/Weapon.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.Buffer;
 
 /**
  * The weapon class is responsible for keeping track of
