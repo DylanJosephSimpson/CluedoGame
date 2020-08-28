b8595967c3f9cebca5e29d1101a70eee6576afcd
Dylan Simpson <65699922+DylanJosephSimpson@users.noreply.github.com>
2020-08-28 12:23:39 +1200

    Update and rename ModelAccusationSetup.java to AccusationSetup.java

--- CluedoGame/View/AccusationSetup.java
+++ CluedoGame/View/AccusationSetup.java
@@ -1,3 +1,7 @@
+package View;
+
+import Model.*;
+import Model.Board;
 import javax.swing.*;
 import java.awt.*;
 
@@ -6,15 +10,15 @@ public class AccusationSetup {
     private JPanel MakeAccusationWindow;
 
     String[] Weapon = new String[]{"Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Spanner"};
-    private JLabel WeaponBoxDesc = new JLabel("Weapon Choice");
+    private JLabel WeaponBoxDesc = new JLabel("Model.Weapon Choice");
     private JComboBox<String> WeaponSelection = new JComboBox<>(Weapon);
 
     String[] Character =  new String[]{"Miss Scarlett", "Colonel Mustard", "Mr. Green", "Ms. White", "Mrs. Peacock", "Professor Plum"};
-    private JLabel CharacterBoxDesc = new JLabel("Character Choice");
+    private JLabel CharacterBoxDesc = new JLabel("Model.Character Choice");
     private JComboBox<String> CharacterSelection  = new JComboBox<>(Character);
 
-    String[] Room = new String[]{"Kitchen", "Ball Room", "Conservatory", "Billiard Room", "Library", "Study", "Hall", "Lounge", "Dining Room"};
-    private JLabel RoomBoxDesc = new JLabel("Weapon Choice");
+    String[] Room = new String[]{"Kitchen", "Ball Model.Room", "Conservatory", "Billiard Model.Room", "Library", "Study", "Hall", "Lounge", "Dining Model.Room"};
+    private JLabel RoomBoxDesc = new JLabel("Model.Weapon Choice");
     private JComboBox<String> RoomSelection  = new JComboBox<>(Room);
 
     public AccusationSetup(Player player){
@@ -36,8 +40,8 @@ public class AccusationSetup {
                 "Make an Accusation", JOptionPane.OK_CANCEL_OPTION);
         if (result == JOptionPane.OK_OPTION){
             //Making an accusation using the selected Cards from the window
-            player.makeAccusation( Board.cardHashMap.get(CharacterSelection.getSelectedItem().toString())
-                    ,Board.cardHashMap.get(WeaponSelection.getSelectedItem().toString()), Board.cardHashMap.get(RoomSelection.getSelectedItem().toString()));
+            player.makeAccusation( Board.getCardHashMap().get(CharacterSelection.getSelectedItem().toString())
+                    ,Board.getCardHashMap().get(WeaponSelection.getSelectedItem().toString()), Board.getCardHashMap().get(RoomSelection.getSelectedItem().toString()));
 
 
             
