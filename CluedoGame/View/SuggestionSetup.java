b5e1fcee1a8073c2ca655e3312d64f8820afdabe
Dylan Simpson <65699922+DylanJosephSimpson@users.noreply.github.com>
2020-08-29 20:54:02 +1200

    Update and rename SuggestionWindow.java to SuggestionSetup.java

---
+++ CluedoGame/View/SuggestionSetup.java
@@ -0,0 +1,51 @@
+package View;
+
+import Model.*;
+import Model.Board;
+import javax.swing.*;
+import java.awt.*;
+
+public class SuggestionSetup {
+
+    private JPanel MakeSuggestionWindow;
+
+    String[] Weapon = new String[]{"Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Spanner"};
+    private JLabel WeaponBoxDesc = new JLabel("Model.Weapon Choice");
+    private JComboBox<String> WeaponSelection = new JComboBox<>(Weapon);
+
+    String[] Character =  new String[]{"Miss Scarlett", "Colonel Mustard", "Mr. Green", "Ms. White", "Mrs. Peacock", "Professor Plum"};
+    private JLabel CharacterBoxDesc = new JLabel("Model.Character Choice");
+    private JComboBox<String> CharacterSelection  = new JComboBox<>(Character);
+
+    String[] Room = new String[]{"Kitchen", "Ball Model.Room", "Conservatory", "Billiard Model.Room", "Library", "Study", "Hall", "Lounge", "Dining Model.Room"};
+    private JLabel RoomBoxDesc = new JLabel("Model.Weapon Choice");
+    private JComboBox<String> RoomSelection  = new JComboBox<>(Room);
+
+    public SuggestionSetup(Player player){
+
+        MakeSuggestionWindow = new JPanel();
+        GridLayout layout = new GridLayout(3, 2);
+        MakeSuggestionWindow.setLayout(layout);
+        MakeSuggestionWindow.add(WeaponBoxDesc);
+        MakeSuggestionWindow.add(WeaponSelection);
+        MakeSuggestionWindow.add(CharacterBoxDesc);
+        MakeSuggestionWindow.add(CharacterSelection);
+        MakeSuggestionWindow.add(RoomBoxDesc);
+        MakeSuggestionWindow.add(RoomSelection);
+
+        int result = JOptionPane.showConfirmDialog(null, MakeSuggestionWindow,
+                "Make an Accusation", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION){
+            //Making an accusation using the selected Cards from the window
+            player.makeSuggestion(
+                    (CharacterCard) Board.getCardHashMap().get(CharacterSelection.getSelectedItem()),
+                    (WeaponCard) Board.getCardHashMap().get(WeaponSelection.getSelectedItem()),
+                    (RoomCard) Board.getCardHashMap().get(RoomSelection.getSelectedItem()))
+            ;
+            System.out.println(WeaponSelection.getSelectedItem() + "\n" + CharacterSelection.getSelectedItem() + "\n" + RoomSelection.getSelectedItem() + "\n");
+        }
+    }
+
+
+
+}
