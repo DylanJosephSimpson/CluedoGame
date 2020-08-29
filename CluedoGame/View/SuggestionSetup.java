d8794e233cdf52c83c63bd62530713745be6c62e
Dylan Simpson <65699922+DylanJosephSimpson@users.noreply.github.com>
2020-08-30 01:08:57 +1200

    Update SuggestionSetup.java

--- CluedoGame/View/SuggestionSetup.java
+++ CluedoGame/View/SuggestionSetup.java
@@ -9,20 +9,15 @@ public class SuggestionSetup {
 
     private JPanel MakeSuggestionWindow;
 
-    String[] Weapon = new String[]{"Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Spanner"};
+    String[] Weapon = new String[]{"Candlestick", "Dagger", "LeadPipe", "Revolver", "Rope", "Spanner"};
     private JLabel WeaponBoxDesc = new JLabel("Weapon Choice");
     private JComboBox<String> WeaponSelection = new JComboBox<>(Weapon);
 
-    String[] Character =  new String[]{"Miss Scarlett", "Colonel Mustard", "Mr. Green", "Ms. White", "Mrs. Peacock", "Professor Plum"};
+    String[] Character =  new String[]{"Miss. Scarlett", "Col. Mustard", "Mr. Green", "Mrs. White", "Mrs. Peacock", "Prof. Plum"};
     private JLabel CharacterBoxDesc = new JLabel("Character Choice");
     private JComboBox<String> CharacterSelection  = new JComboBox<>(Character);
 
-    String[] Room = new String[]{"Kitchen", "Ballroom", "Conservatory", "Billiard Room", "Library", "Study", "Hall", "Lounge", "Dining Room"};
-    private JLabel RoomBoxDesc = new JLabel("Weapon Choice");
-    private JComboBox<String> RoomSelection  = new JComboBox<>(Room);
-
     public SuggestionSetup(Player player){
-
         MakeSuggestionWindow = new JPanel();
         GridLayout layout = new GridLayout(3, 2);
         MakeSuggestionWindow.setLayout(layout);
@@ -30,19 +25,18 @@ public class SuggestionSetup {
         MakeSuggestionWindow.add(WeaponSelection);
         MakeSuggestionWindow.add(CharacterBoxDesc);
         MakeSuggestionWindow.add(CharacterSelection);
-        MakeSuggestionWindow.add(RoomBoxDesc);
-        MakeSuggestionWindow.add(RoomSelection);
 
         int result = JOptionPane.showConfirmDialog(null, MakeSuggestionWindow,
                 "Make an Accusation", JOptionPane.OK_CANCEL_OPTION);
         if (result == JOptionPane.OK_OPTION){
             //Making an accusation using the selected Cards from the window
-            player.makeSuggestion(
-                    (CharacterCard) Board.getCardHashMap().get(CharacterSelection.getSelectedItem()),
-                    (WeaponCard) Board.getCardHashMap().get(WeaponSelection.getSelectedItem()),
-                    (RoomCard) Board.getCardHashMap().get(RoomSelection.getSelectedItem()))
-            ;
-            System.out.println(WeaponSelection.getSelectedItem() + "\n" + CharacterSelection.getSelectedItem() + "\n" + RoomSelection.getSelectedItem() + "\n");
+            Suggestion currentSuggetion = new Suggestion(
+                    Board.getCharacterFromString((String) CharacterSelection.getSelectedItem()),
+                    Board.getWeaponFromString( (String) WeaponSelection.getSelectedItem()),
+                    Player.findPlayerRoom(player.getAssignedCharacter().getX(), player.getAssignedCharacter().getY())
+            );
+            currentSuggetion.moveItems();
+            System.out.println(WeaponSelection.getSelectedItem() + "\n" + CharacterSelection.getSelectedItem() + "\n" + player.findPlayerRoom(player.getAssignedCharacter().getX(), player.getAssignedCharacter().getY() ) + "\n");
         }
     }
 }
