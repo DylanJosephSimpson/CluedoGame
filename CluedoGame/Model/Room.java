dee9c7f62890e321c9db0e7301de8ca5d85b234b
Praveen Bandarage <51874907+PraveentheMachine@users.noreply.github.com>
2020-08-31 12:22:54 +1200

    Update Room.java

--- CluedoGame/Model/Room.java
+++ CluedoGame/Model/Room.java
@@ -50,7 +50,7 @@ public class Room {
     }
 
     /**
-     *  --- Model.Suggestion Method ---
+     *  --- Suggestion Method ---
      * Add a weapon to this room (if it is not already in it) when a suggestion is made.
      * @param weapon
      */
@@ -59,7 +59,7 @@ public class Room {
     }
 
     /**
-     * --- Model.Suggestion Method ---
+     * --- Suggestion Method ---
      * Remove the weapon from this room when the weapon has to be moved into a different room
      * @param weapon
      */
@@ -68,7 +68,7 @@ public class Room {
     }
 
     /**
-     *  --- Model.Suggestion Method ---
+     *  --- Suggestion Method ---
      * Add a character to this room (if it is not already in it) when a suggestion is made
      * Also remove the character from its previous room.
      * @param character
@@ -78,7 +78,7 @@ public class Room {
     }
 
     /**
-     * --- Model.Suggestion Method ---
+     * --- Suggestion Method ---
      * Remove the weapon from this room when the weapon has to be moved into a different room
      * @param character
      */
@@ -86,42 +86,65 @@ public class Room {
         charactersInRoom.remove(character);
     }
 
+    /** Check to see if the Room is involved with the murder
+     */
     public boolean isInvolvedInMurder() {
         return isInvolvedInMurder;
     }
 
-    public void setInvolvedInMurder(boolean involvedInMurder) {
-        isInvolvedInMurder = involvedInMurder;
-    }
-
+    /**
+     * Getter for room nanme
+     * @return the Room's name
+     */
     public String getRoomName() {
         return roomName;
     }
+
+    /**
+     * Getting the room tiles
+     * @return the list of room-only tiles
+     */
 
     public List<Tile> getRoomTiles() {
         return roomTiles;
     }
 
+    /**
+     * Adding a tile to the room
+     * @param tile the tile to be added
+     */
     public void addRoomTile(Tile tile){
         roomTiles.add(tile);
     }
 
+    /**
+     * Getting the tiles associated with the doorway
+     * @return the set of doorway tiles
+     */
     public Set<Tile> getDoorwayTiles() {
         return doorwayTiles;
     }
 
-    public void setDoorwayTiles(Set<Tile> doorwayTiles) {
-        this.doorwayTiles = doorwayTiles;
-    }
-
+    /**
+     * Method for adding to a doorway
+     * @param doorwayTile the doorway tiles
+     */
     public void addDoorWay(Tile doorwayTile){
         doorwayTiles.add(doorwayTile);
     }
 
+    /**
+     * Getter for the weapons in a room
+     * @return the weapons in the room
+     */
     public List<Weapon> getWeaponsInRoom() {
         return weaponsInRoom;
     }
 
+    /**
+     * Method for getting the characters in a room
+     * @return
+     */
     public List<Character> getCharactersInRoom() {
         return charactersInRoom;
     }
